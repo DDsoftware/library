0fd44952e09605e28c169d4f60a09c24eebf83ae
Dramane Diakite <dramane.diakite11@gmail.com>
2024-10-28 18:45:34 -0400

    initial commit

---
+++ src/main/java/com/dramaneAcademy/library/DramaneAcademyLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.dramaneAcademy.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaneAcademyLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaneAcademyLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dramaneAcademy/library/DramaneAcademyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dramaneAcademy.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramaneAcademyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
